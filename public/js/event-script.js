dd5aca594cef769dcbf9377bc347031638a02005
Jose Mendoza <joseemendoza194@gmail.com>
2019-12-21 16:20:46 -0800

    Added event script

--- public/js/event-script.js
+++ public/js/event-script.js
@@ -1,3 +1,5 @@
+console.log('Event scrpit loaded!')
+
 function init() {
 	var coll = document.getElementsByClassName("collapsible");
 	var i;
@@ -13,4 +15,23 @@ function init() {
 			}
 	  	});
 	}
+}
+
+document.forms["event-creation"].addEventListener('submit', (event)=> createEvent(event))
+
+function createEvent(event){
+	event.preventDefault()
+
+	const eventObj = {
+		description: document.getElementById('event-description').value,
+		location: document.getElementById('event-location').value,
+		time: document.getElementById('event-time').value
+	}
+
+	const jsonData = JSON.stringify(eventObj);
+
+	var xhr = new XMLHttpeRequest();
+	xhr.open('POST', '/events', true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.send(jsonData);
 }
