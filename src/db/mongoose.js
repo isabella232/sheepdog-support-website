ad34388edffd8d0233986773b6b4e72a6fdfa660
Jose Mendoza <joseemendoza194@gmail.com>
2019-12-26 00:02:59 -0800

    Setting up environment variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
+//mongodb+srv://bitPrj:Veteran123@sheepdog-support-da8gp.mongodb.net/test?retryWrites=true
+
 const connectionURL = 'mongodb://127.0.0.1:27017'
-mongoose.connect(connectionURL + '/sheepdog-support-api', {
+mongoose.connect(process.env.MONGODB_URI + '/sheepdog-support-api', {
     useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: false,
