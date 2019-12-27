fc0aac1288bf1ed65df7e008915fe285ae69524d
Jose Mendoza <joseemendoza194@gmail.com>
2019-12-26 20:24:05 -0800

    Found user coordinates in google maps

--- public/js/directory.js
+++ public/js/directory.js
@@ -1,10 +1,52 @@
 console.log('Directory Script LOADED!')
 
 function initMap(){
-      const map = new google.maps.Map(
+    const map = new google.maps.Map(
         document.getElementById('map'), {
             center: {lat: -34.397, lng: 150.644},
             zoom: 8
+        }); 
+
+    const infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+        }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
         });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    
+
+    // const marker = new google.maps.Marker({
+    //     Position:pos,
+    //     map:map,
+    //     title: 'My position'
+    // })
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
       // The marker, positioned at Uluru
+}
+
+const currentPos = () =>{
+    
 }
